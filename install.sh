0633386767a516e93f2a1b4515acdb6a4b166a2c
mauriciodev <mauricio.dev@gmail.com>
2021-06-15 14:22:51 -0300

    Froze because the colab is not working as expected.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ conda create -p `pwd`/autodeeplab -c conda-forge
 conda activate `pwd`/autodeeplab
 conda activate "autodeeplab"
 conda install -c conda-forge tensorflow-gpu=="1.15" pillow tf_slim jupyterlab
-
+conda install -c conda-forge gdal matplotlib scikit-learn
 
 conda remove --force cudnn
 conda remove --force cudatoolkit
--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,37 +1,44 @@
 import numpy as np
 from osgeo import ogr, gdal, gdal_array
-import tensorflow as tf
 import os,sys,math
 import matplotlib.pyplot as plt
 from PIL import Image
-
+from sklearn import model_selection
 #https://rockyshikoku.medium.com/train-deeplab-v3-with-your-own-dataset-13f2af958a75
 
 
-"""Adds a border of (patch_size-stride)/2 and slices the input array."""
-def extract_patches(inputArray, patch_size, stride): #returns both the list of patches and an ordered set of patches.
-    border_size=int((patch_size-stride)/2)
-    originalShape=inputArray.shape
-    maxI=math.ceil(originalShape[1]/stride)
-    maxJ=math.ceil(originalShape[0]/stride)
-    extraPadJ=maxJ*stride-originalShape[0]
-    extraPadI=maxI*stride-originalShape[1]
-    image=np.pad(inputArray,((border_size,border_size+extraPadJ),(border_size,border_size+extraPadI),(0,0)),mode='symmetric')
-    gdal_array.SaveArray(np.moveaxis(image, -1, 0),os.path.join(os.getcwd(),'teste_pad.tif'))
-    #print(image.shape)
-    image = tf.expand_dims(image,0) # To create the batch information
-    patches = tf.image.extract_patches(image,[1,patch_size,patch_size,1],[1,stride,stride,1], rates=[1, 1, 1, 1],padding="VALID")
-    return tf.reshape(patches, shape=[-1, patch_size, patch_size, image.shape[3]])
-
-def extract_patches1(inputArray, patch_size, stride): #returns both the list of patches and an ordered set of patches.
-    border_size=int((patch_size-stride)/2)
-    originalShape=inputArray.shape
-    maxI=math.ceil(originalShape[1]/stride)
-    maxJ=math.ceil(originalShape[0]/stride)
-    #print(image.shape)
+"""def extract_patches(inputArray, patch_size, stride): #returns both the list of patches and an ordered set of patches.
     image = tf.expand_dims(inputArray,0) # To create the batch information
-    patches = tf.image.extract_patches(image,[1,patch_size,patch_size,1],[1,stride,stride,1], rates=[1, 1, 1, 1],padding="VALID")
-    return tf.reshape(patches, shape=[-1, patch_size, patch_size, image.shape[3]])
+    patches = tf.image.extract_image_patches(image,[1,patch_size,patch_size,1],[1,stride,stride,1], rates=[1, 1, 1, 1],padding="VALID")
+    patches=tf.reshape(patches, shape=[-1, patch_size, patch_size, image.shape[3]])
+    res=[]
+    for patch_id in range(patches.shape[0]):
+        res.append(np.array(patches[1].eval(session=session)))
+    tf.reset_default_graph()
+    return res
+"""
+def extract_patches1(imgArray, patch_size, stride):
+  # Read raster data as numeric array from file
+  print(imgArray.shape)
+  
+  border_size=int((patch_size-stride)/2)
+  #let's mirror the borders so we can actually run the entire image
+  #imgArray2=np.pad(imgArray,((0,0),(border_size,border_size),(border_size,border_size)),mode='reflect')
+  x_max,y_max,channels=imgArray.shape
+  imagesArr=[]
+  x=0
+  while x_max>x+patch_size:
+    y=0
+    x_0=x
+    x_1=x_0+patch_size
+    while y_max>y+patch_size:
+        y_0=y
+        y_1=y_0+patch_size
+        subArray=imgArray[x_0:x_1,y_0:y_1,:]
+        imagesArr.append(subArray)
+        y+=stride
+    x+=stride
+  return imagesArr
 
 def rebuildImage(patches,originalShape,patch_size,stride):
     border_size=int((patch_size-stride)/2)
@@ -45,42 +52,73 @@ def rebuildImage(patches,originalShape,patch_size,stride):
     resImage=tf.image.crop_to_bounding_box(resImage,0,0,originalShape[0],originalShape[1])
     return resImage
 
-if __name__=="__main__":
+#if __name__=="__main__":
 
 
-    patch_size = 256
-    stride = 256-32 #using 32 pixels of overlap
+patch_size = 256
+stride = 256-32 #using 32 pixels of overlap
 
-    imagesId=[3,4,7,8]
-    band_images_pattern="TLS_BDSD_NIRRG/TLS_BDSD_NIRRG_{:02d}.tif"
-    label_images_pattern="TLS_GT/TLS_GT_{:02d}.tif"
+imagesId=[3,4]
+band_images_pattern="TLS_BDSD_NIRRG/TLS_BDSD_NIRRG_{:02d}.tif"
+label_images_pattern="TLS_indMap/TLS_indMap_{:02d}.tif"
+
+outputdir=os.getcwd()+"/TLS"
+
+#for imageName in os.listdir():
+imageList=[]
+
+
+for imageid in imagesId:
+    band_image=band_images_pattern.format(imageid)
+    label_image=label_images_pattern.format(imageid)
+
+    if not (os.path.exists(band_image) and os.path.exists(label_image)):
+        print(f"Some images were not found: {band_image} {label_image}")
+        continue
+    baseFileName=os.path.split(band_image)[-1][:-4]
+
+    bandArray = gdal_array.LoadFile(band_image)
+    bandArray = np.moveaxis(bandArray, 0, -1)
+    band_patches=extract_patches1(bandArray,patch_size,stride)
+    labelsArray = gdal_array.LoadFile(label_image) #this is a grayscale image
+    labelsArray = labelsArray.reshape(labelsArray.shape+(1,))
+    #labelsArray = np.moveaxis(labelsArray, 0, -1)
+    label_patches=extract_patches1(labelsArray,patch_size,stride)
     
-    outputdir=os.getcwd()+"/JPEG"
+    classRawDir=os.path.join(outputdir,'SegmentationClassRaw')
+    jpegImagesDir=os.path.join(outputdir,'JPEGImages')
+    imageSetDir=os.path.join(outputdir,'ImageSet')
+    os.makedirs(classRawDir, exist_ok=True)
+    os.makedirs(jpegImagesDir, exist_ok=True)
+    os.makedirs(imageSetDir, exist_ok=True)
+    print(len(band_patches))
     
-    #for imageName in os.listdir():
-    for id in imagesId:
-        band_image=band_images_pattern.format(id)
-        label_image=label_images_pattern.format(id)
 
-        if not (os.path.exists(band_image) and os.path.exists(label_image)):
-            print(f"Some images were not found: {band_image} {label_image}")
-            continue
-        baseFileName=os.path.split(band_image)[-1][:-4]
+    
+    for patch_id in range(len(band_patches)):
 
-        bandArray = gdal_array.LoadFile(band_image)
-        bandArray = np.moveaxis(bandArray, 0, -1)
-        band_patches=extract_patches1(bandArray,patch_size,stride)
-        labelsArray = gdal_array.LoadFile(label_image)
-        labelsArray = np.moveaxis(labelsArray, 0, -1)
-        label_patches=extract_patches1(labelsArray,patch_size,stride)
-        for patch_id, patch_array in enumerate(band_patches):
-            #outputImage = np.moveaxis(patch_array, -1, 0)
-            im = Image.fromarray(np.array(patch_array))
-            im.save(os.path.join(outputdir,f'{baseFileName}_{patch_id}_.jpg'), quality=100, subsampling=0)
-            im = Image.fromarray(np.array(label_patches[patch_id]))
-            im.save(os.path.join(outputdir,f'{baseFileName}_{patch_id}_ref_.jpg'), quality=100, subsampling=0)
-            #gdal_array.SaveArray(outputImage,os.path.join(outputdir,f'{baseFileName}_{patch_id}_.jpg'),  format="JPEG")
-
+        patch_array=np.array(band_patches[patch_id])
+        imageId=f'{baseFileName}_{patch_id:03}'
+        imageList.append(imageId)
+        #outputImage = np.moveaxis(patch_array, -1, 0)
+        im = Image.fromarray(patch_array)
+        im.save(os.path.join(jpegImagesDir,imageId+'.png'), quality=100, subsampling=0)
+        grayPatch=np.array(label_patches[patch_id]) #returning to 2D because it's grayscale
+        im = Image.fromarray(np.squeeze(grayPatch) , 'L') 
+        im.save(os.path.join(classRawDir,imageId+'.png'), quality=100, subsampling=0)
+        #gdal_array.SaveArray(outputImage,os.path.join(outputdir,f'{baseFileName}_{patch_id}_.jpg'),  format="JPEG")
+    
+    with open(os.path.join(imageSetDir,'trainval.txt'),'w') as f:
+        for image in imageList:
+            f.write(image+'\n')
+    #train.txt  trainval.txt  val.txt
+    (train, val) = model_selection.train_test_split(imageList, test_size=0.20, random_state=42)
+    with open(os.path.join(imageSetDir,'train.txt'),'w') as f:
+        for image in train:
+            f.write(image+'\n')
+    with open(os.path.join(imageSetDir,'val.txt'),'w') as f:
+        for image in val:
+            f.write(image+'\n')
     
     #this is just an example of how to restore the images
     #outputImage=rebuildImage(band_patches,bandArray.shape,patch_size,stride)
